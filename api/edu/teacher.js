d34e221fcf472a2843ad408511593b7077f5a538
温文星 <18475536452@163.com>
2020-03-11 15:51:12 +0800

    前端讲师列表页接口联调

---
+++ api/edu/teacher.js
@@ -0,0 +1,10 @@
+import request from '@/utils/request'
+const api_name = '/front/teacher'
+export default {
+  getPageList(page, limit) {
+    return request({
+      url: `${api_name}/${page}/${limit}`,
+      method: 'get'
+    })
+  }
+}
--- utils/request.js
+++ utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // 创建axios实例
 const service = axios.create({
-  baseURL: 'http://localhost:9001', // api的base_url
+  baseURL: 'http://localhost:9000', // api的base_url
   timeout: 30000 // 请求超时时间
 })
 export default service
